7eebbcc1c5e82095c8329a49fc9f5323aa6b57c1
Deepam-Ameria <ameria.deepam@gmail.com>
2026-03-18 20:08:11 -0400

    Added argparse to parametrize train.py. Upgraded pytorch

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import env.nav_env
 import gymnasium as gym
 import wandb
@@ -5,7 +6,6 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.vec_env import DummyVecEnv
-
 
 
 class WandbCallback(BaseCallback):
@@ -17,25 +17,24 @@ class WandbCallback(BaseCallback):
                 wandb.log({
                     "episode_reward": info["episode"]["r"],
                     "episode_length": info["episode"]["l"],
-                    "success": float(info.get("success", 0))
+                    "success": float(info.get("success", 0)),
+                    "timestep": self.num_timesteps,
                 })
 
         return True
 
 
-def main():
+def main(args):
     wandb.init(
         project="vla-rl-nav",
-        name="ppo_phase1_baseline",
+        name=args.run_name,
         config={
             "algo": "PPO",
-            "total_timesteps": 500_000
+            "total_timesteps": args.timesteps,
+            "n_envs": args.n_envs,
+            "device": args.device,
         }
     )
-
-    # env = gym.make("NavEnv-v0")
-    # env = Monitor(env)
-
 
     def make_env():
         def _init():
@@ -43,24 +42,31 @@ def main():
             return Monitor(env)
         return _init
 
-    env = DummyVecEnv([make_env() for _ in range(8)])
+    env = DummyVecEnv([make_env() for _ in range(args.n_envs)])
 
     model = PPO(
         "MlpPolicy",
         env,
         verbose=1,
         tensorboard_log="./logs/",
-        device = "cpu"
+        device=args.device,
     )
 
     model.learn(
-        total_timesteps=500_000,
-        callback=WandbCallback()
+        total_timesteps=args.timesteps,
+        callback=WandbCallback(),
     )
 
-    model.save("ppo_nav_model")
+    model.save(args.save_path)
     wandb.finish()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--timesteps", type=int, default=500_000)
+    parser.add_argument("--n-envs", type=int, default=8)
+    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--run-name", type=str, default="ppo_baseline")
+    parser.add_argument("--save-path", type=str, default="ppo_nav_model")
+    args = parser.parse_args()
+    main(args)
